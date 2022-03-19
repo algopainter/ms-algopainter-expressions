671710fae233c615500fc11dfde734d6c91943c8
Rodrigo Cour <rrcoura@gmail.com>
2022-03-19 14:36:37 -0300

    Attempt to fix netlfy with expression 1

--- netlify/express.js
+++ netlify/express.js
@@ -17,7 +17,7 @@ module.exports.handler = async function(event, context) {
       const wireframeBlendStyle = event.queryStringParameters.wireframeBlendStyle;
       const size = event.queryStringParameters.size;
       const flip = event.queryStringParameters.flip === 'true';
-      const dir = event.queryStringParameters.dir || '.';
+      const dir = event.queryStringParameters.dir;
         
       const base = await paint(dir, {
         background,
